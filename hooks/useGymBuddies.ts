eb88d9b0808cf20c70d7c28ba080ac41e8483e4e
Selim Fedakar <a.selimfedakar@gmail.com>
2026-07-08 12:20:31 -0700

    hooks: pass gym discovery filters

--- hooks/useGymBuddies.ts
+++ hooks/useGymBuddies.ts
@@ -1,16 +1,16 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { fetchGymBuddies } from '@/lib/queries/discovery'
+import { fetchGymBuddies, type GymFilters } from '@/lib/queries/discovery'
 import { useAuthStore } from '@/store/auth'
 
 export const GYM_PAGE_SIZE = 20
 
-export function useGymBuddies(radiusKm = 50, minFrequency = 1) {
+export function useGymBuddies(radiusKm = 50, minFrequency = 1, filters: GymFilters = {}) {
   const userId = useAuthStore((s) => s.session?.user.id)
 
   return useInfiniteQuery({
-    queryKey:         ['gym-buddies', userId, radiusKm, minFrequency],
+    queryKey:         ['gym-buddies', userId, radiusKm, minFrequency, filters.sameUniversity, filters.experience],
     queryFn:          ({ pageParam }) =>
-      fetchGymBuddies(userId!, radiusKm, minFrequency, GYM_PAGE_SIZE, pageParam as number),
+      fetchGymBuddies(userId!, radiusKm, minFrequency, GYM_PAGE_SIZE, pageParam as number, filters),
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) =>
       lastPage.length < GYM_PAGE_SIZE ? undefined : allPages.length * GYM_PAGE_SIZE,
