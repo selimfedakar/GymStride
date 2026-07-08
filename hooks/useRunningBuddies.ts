c9b788bbc4d636fc67a71f9c8202d56be4ba4d0e
Selim Fedakar <a.selimfedakar@gmail.com>
2026-07-08 12:20:33 -0700

    hooks: pass running discovery filters

--- hooks/useRunningBuddies.ts
+++ hooks/useRunningBuddies.ts
@@ -1,17 +1,17 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
-import { fetchRunningBuddies } from '@/lib/queries/discovery'
+import { fetchRunningBuddies, type RunningFilters } from '@/lib/queries/discovery'
 import { useAuthStore } from '@/store/auth'
 import type { RunType } from '@/types/database'
 
 export const RUN_PAGE_SIZE = 20
 
-export function useRunningBuddies(radiusKm = 50, runType: RunType | null = null) {
+export function useRunningBuddies(radiusKm = 50, runType: RunType | null = null, filters: RunningFilters = {}) {
   const userId = useAuthStore((s) => s.session?.user.id)
 
   return useInfiniteQuery({
-    queryKey:         ['running-buddies', userId, radiusKm, runType],
+    queryKey:         ['running-buddies', userId, radiusKm, runType, filters.sameUniversity, filters.maxPaceSeconds],
     queryFn:          ({ pageParam }) =>
-      fetchRunningBuddies(userId!, radiusKm, runType, RUN_PAGE_SIZE, pageParam as number),
+      fetchRunningBuddies(userId!, radiusKm, runType, RUN_PAGE_SIZE, pageParam as number, filters),
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages) =>
       lastPage.length < RUN_PAGE_SIZE ? undefined : allPages.length * RUN_PAGE_SIZE,
